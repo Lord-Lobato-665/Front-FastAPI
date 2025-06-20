4bcedd6c0bf274541f238f069f05c967e40bccae
JosueACG17 <cgjosue17@gmail.com>
2025-06-20 14:37:12 -0500

    feat(ui): aplicar diseño completo con estilo mejorado a componente de predicción de salud mental

--- src/views/LinearRegression.vue
+++ src/views/LinearRegression.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="container mx-auto p-6">
-    <h1 class="text-4xl font-bold mb-6 mt-3 text-gray-800 text-center">Regresión Lineal</h1>
       <MentalHealthPrediction />
-      </div>
 </template>
 
 <script lang="ts">
